65c2e6db3d79791ec9c2e9714ac3df078e3e0204
Andy Williams <andy@andy.xyz>
2024-02-27 14:38:09 +0000

    Implement a search sorter with some special cases falling back to levenshtein

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,12 +2,18 @@ $(function () {
 
   function initSearchBox() {
     var pages = new Bloodhound({
-      //datumTokenizer: Bloodhound.tokenizers.obj.whitespace('title'),
       datumTokenizer: Bloodhound.tokenizers.obj.nonword(['title','content']),
       queryTokenizer: Bloodhound.tokenizers.whitespace,
+      sorter: function(a, b) {
+        var input_string = $('#search-box').val();
+        return distance(a.title, input_string) - distance(b.title, input_string);
+      },
 
       prefetch: {
         url: '/search.json',
+        transform : function(response) {
+          return pages.sorter(response)
+        },
         cache: false
       }
     });
